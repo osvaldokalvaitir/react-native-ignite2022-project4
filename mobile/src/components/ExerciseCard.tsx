c663720d81ca1fef63658117e278d2333fd71134
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-11-05 12:18:09 -0300

    Carregando imagem

--- mobile/src/components/ExerciseCard.tsx
+++ mobile/src/components/ExerciseCard.tsx
@@ -2,6 +2,8 @@ import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
 import { Heading, HStack, Image, Text, VStack, Icon } from 'native-base';
 import { Entypo } from '@expo/vector-icons';
 
+import { api } from '@services/api';
+
 import { ExerciseDTO } from '@dtos/ExerciseDTO';
 
 type Props = TouchableOpacityProps & {
@@ -13,7 +15,7 @@ export function ExerciseCard({ data, ...rest }: Props) {
     <TouchableOpacity {...rest}>
       <HStack bg="gray.500" alignItems="center" p={2} pr={4} rounded="md" mb={3}>
         <Image 
-          source={{ uri: 'https://www.origym.com.br/upload/remada-unilateral-3.png' }}
+          source={{ uri: `${api.defaults.baseURL}/exercise/thumb/${data.thumb}` }}
           alt="Imagem do exercício"
           w={16}
           h={16}

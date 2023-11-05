33f6f7d3bf5fd276657d8ba3fd5c689a74f52304
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-11-05 11:46:14 -0300

    Buscando e Listando Grupos

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { AppError } from "@utils/AppError";
 
 const api = axios.create({
-  baseURL: 'http://172.21.176.1:3333'
+  baseURL: 'http://172.29.192.1:3333'
 });
 
 api.interceptors.response.use((response) => response, (error) => {

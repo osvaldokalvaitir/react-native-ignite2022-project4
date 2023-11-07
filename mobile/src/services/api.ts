ede34bb29d3b5d24243c7d776b4dc8dce049da74
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-11-07 20:46:01 -0300

    Armazenando o Refresh Token

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 import { AppError } from "@utils/AppError";
+import { storageAuthTokenGet } from "@storage/storageAuthToken";
 
 type SignOut = () => void;
 
@@ -13,9 +14,15 @@ const api = axios.create({
 }) as APIInstanceProps;
 
 api.registerInterceptTokenManager = singOut => {
-  const interceptTokenManager = api.interceptors.response.use((response) => response, requestError => {
+  const interceptTokenManager = api.interceptors.response.use((response) => response, async (requestError) => {
     if (requestError.response?.status === 401) {
       if (requestError.response.data?.message === 'token.expired' || requestError.response.data?.message === 'token.invalid') {
+        const { refresh_token } = await storageAuthTokenGet();
+
+        if (!refresh_token) {
+          singOut();
+          return Promise.reject(requestError)
+        }
       }
 
       singOut();
